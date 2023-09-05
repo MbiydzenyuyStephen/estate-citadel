d619c814467a6b1393d7ae07191e54b85dc80b9d
StephJr2000 <mbiystephjr2gmail.com>
2023-09-05 09:39:41 +0100

    Signup Page

---
+++ src/components/signup.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+class Signup extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <h1>SignUp</h1>
+        );
+    }
+}
+ 
+export default Signup;
