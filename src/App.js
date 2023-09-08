d0c850c597b983deaaaded4ae34a8a228ba9419b
StephJr2000 <mbiystephjr2gmail.com>
2023-09-08 14:41:49 +0100

    All requested changes have been addressed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Signup from './components/signup/SignUp';
-import Signin from './components/signin/SignIn';
+import Signup from './components/signup/signup';
+import Signin from './components/signin/signin';
 
 
 function App() {
