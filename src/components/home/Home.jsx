644433f321cd07980c433c61ffe332983adee851
StephJr2000 <mbiystephjr2gmail.com>
2023-09-12 17:40:12 +0100

    Home page added

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -7,27 +7,23 @@ const Home = () => {
   return (
     <div className="parent-div">
       <NavBar />
-
-      <h1 className="search-area"></h1>
-
+      
       <div className="search-section">
         <div class="row">
           <div class="col-md-8">
+          <h1 className="search-section-header">Discover a place you'll love to live</h1>
+
             <div class="input-group mb-3">
               <input
                 type="text"
                 class="form-control input-text"
-                placeholder="Search products...."
-                
-                />
-                <FaSistrix/>
-              </div>
-            
-              
+                placeholder="Search location, Neighborhooh, City, Quater..."
+              />
             </div>
           </div>
         </div>
       </div>
+    </div>
   );
 };
 
