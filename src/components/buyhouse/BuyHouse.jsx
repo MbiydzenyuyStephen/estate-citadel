6cea830d16d1363584a3a73b2baf2e1c637a7a80
StephJr2000 <mbiystephjr2gmail.com>
2023-11-23 20:31:59 +0100

    Signup form vallidation, it gave me an error which i was unable to debug

--- src/components/buyhouse/BuyHouse.jsx
+++ src/components/buyhouse/BuyHouse.jsx
@@ -24,33 +24,28 @@ const BuyHouse = () => {
           <div className="col-6 map-col ">
            <Map /> 
           </div>
-           <div className="col-6">
-            <div className="row">
-              <h5>Real Estate & Homes For Sale</h5>
-            </div>
-            <div className="row">
-              <h6 className="d-flex justify-content-start">500 results</h6>
-              <h6 className="d-flex justify-content-end text-primary">
-                sort: Homes for You <FaAngleDown />
-              </h6>
- 
-              <div className="container">
-                <div className="row justify-content-center">
-                  <div className="col-12 col sm-6">
-                    <div className="row mt-5 mb-5">
-                      <div className="col-12 col-sm-2 d-flex justify-content-center property-cards">
-                        <Card />
+              <div className="col-6">
+                <div className="row">
+                  <h5>Real Estate & Homes For Sale</h5>
+                </div>
+                <div className="row">
+                  <h6 className="d-flex justify-content-start">500 results</h6>
+                  <h6 className="d-flex justify-content-end text-primary">
+                    sort: Homes for You <FaAngleDown />
+                  </h6>
+    
+                  <div className="container">
+                    <div className="row col ">
+                      <div className="card-one">
+                        <Card/>
                       </div>
-                      <div className="col-20 col-lg-2 d-flex justify-content-end property-cards">
-                        <Card />
+                      <div className="card-one">
+                        <Card/>
                       </div>
-                      <div className="col-12 col-lg-2 d-flex justify-content-center property-cards"></div>
                     </div>
                   </div>
-                </div>
+               </div>
               </div>
-            </div>
-          </div>
         </div>
         <div className="row">
           <div className="col-lg-2 col-sm-1"></div>
--- src/components/signup/SignUp.jsx
+++ src/components/signup/SignUp.jsx
@@ -1,14 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
+import Joi from "joi";
+import axios from "axios";
 import "./signup.css";
 import logo from "../../assets/logo.png";
 import { Link } from "react-router-dom";
 
 const SignUp = () => {
+
+  const [formData, setFormData] = useState({
+    username: "",
+    fullname: "",
+    email:"",
+    password:"",
+    confirmPassword:"",
+    isLandlord: false,
+    acceptTerms: false,
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const schema = Joi.object({
+    username: Joi.string().aphanum().min(3).max(30).required(),
+    fullname:Joi.string().min().required(),
+    email: Joi.string().email().required(),
+    password:Joi.string().min(6).required(),
+    confirmPassword: Joi.string()
+      .valid(Joi.ref("password"))
+      .required()
+      .label("Confirm Password")
+      .messages({ "any.only ": "{{#label}} does not match"}),
+
+      isLandlord:Joi.boolean().required(),
+      acceptTerms: Joi.boolean().valid(true).reqired(),
+  });
+
+  const handleChange = (e) => {
+    const { name, value, type, checked } = e.target;
+    const fieldValue = type === "checkbox" ? checked : value;
+    setFormData({...formData, [name]: fieldValue });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  
+  try {
+    // i vallidate my forms data
+    const { error } = schema.validate(formData, {abortEarly: false});
+
+    if (error) {
+      const validationErrors = {};
+      for (let item of error.details) {
+        validationErrors[item.path[0]] = item.message;
+      }
+      setErrors(validationErrors);
+      return;
+    }
+
+    //clear any error
+      setErrors({});
+
+      //make API request to submit form data
+      const response = await axios.post("/api/signup", formData);
+      console.log(response.data);//handles success response
+
+      //clear form data after sucessful submission
+      setFormData({
+        username: "",
+        fullname: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+        isLandlord: false,
+        acceptTerms: false,
+      });
+  } catch(error) {
+    console.log(error); // To handle error response
+  }
+};
+
   return (
     <div className="Container sign-up-bg">
       <div className=" row">
         <div className="col-lg-4 offset-lg-4 col-md-6 offset-md-3">
-          <form className="border custom-form-content">
+          <form className="border custom-form-content" onSubmit={handleSubmit
+          }>
             <div className="text-center">
               <img
                 className="signUpLogo image-fluid"
@@ -24,11 +99,17 @@ const SignUp = () => {
                 <span className="font-weight-bold-sign-up">Username</span>
               </label>
               <input
-                type="username"
+                type="text"
                 class="form-control"
                 id="username"
                 placeholder="Enter username"
+                name="username"
+                value={formData.username}
+                onChange={handleChange}
               />
+              {errors.username && ( 
+                <div className="error-message">{errors.username}</div>
+              )}
             </div>
             <div class="form-group">
               <label for="fullname">
@@ -39,8 +120,15 @@ const SignUp = () => {
                 class="form-control"
                 id="fullname"
                 placeholder="Enter full name "
+                name="fullname"
+                value={formData.fullname}
+                onChange={handleChange}
               />
+              {errors.fullname && ( 
+                <div className="error-message">{errors.fullname}</div>
+              )}
             </div>
+
             <div class="form-group">
               <label for="email">
                 <span className="font-weight-bold-sign-up">Email</span>
@@ -50,7 +138,13 @@ const SignUp = () => {
                 class="form-control"
                 id="email"
                 placeholder="Enter email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
               />
+              {errors.email && (
+                <div className="error-message">{errors.email}</div>
+              )}
             </div>
             <div class="form-group">
               <label for="password">
@@ -61,7 +155,13 @@ const SignUp = () => {
                 class="form-control"
                 id="password"
                 placeholder="Enter your password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
               />
+              {errors.password && (
+                <div className="error-message">{errors.password}</div>
+              )}
             </div>
             <div class="form-group">
               <label for="confirmpassword">
@@ -74,10 +174,23 @@ const SignUp = () => {
                 class="form-control"
                 id="confirmpassword"
                 placeholder="Confirm Password"
+                name="confirmPassword"
+                value={formData.confirmPassword}
+                onChange={handleChange}
               />
+              {errors.confirmPassword && (
+                <div className="error-message">{errors.confirmPassword}</div>
+              )}
             </div>
             <div class="form-check mb-3">
-              <input type="checkbox" class="form-check-input" id="landlord" />
+              <input 
+               type="checkbox"
+               class="form-check-input"
+               id="landlord"
+               name="isLandlord"
+                checked={formData.isLandlord}
+                onChange={handleChange}
+                />
               <label class="form-check-label" for="landlord">
                 <span className="font-weight-bold-sign-up">
                   I am a land lord
@@ -89,13 +202,19 @@ const SignUp = () => {
                 type="checkbox"
                 class="form-check-input"
                 id="TermsAndConditions"
+                name="acceptTerms"
+                checked={formData.acceptTerms}
+                onChange={handleChange}
               />
               <label class="form-check-label" for="TermsAndConditions">
                 <span className="font-weight-bold-sign-up">
                   I accept the terms and conditions
                 </span>
               </label>
-            </div>
+              {errors.acceptTerms && (
+                <div className="error-message">{errors.acceptTerms}</div>
+              )}
+              </div>
             <Link to="/">
               <button type="submit" class="btn btn-primary btn-block">
                 Sign Up
